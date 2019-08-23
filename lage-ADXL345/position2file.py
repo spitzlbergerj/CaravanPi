dbadd701af9f416de2ea651b9058b115909a715d
Josef Spitzlberger <josef@spitzlberger.de>
2019-08-23 16:27:06 +0200

    Add skripts

---
+++ lage-ADXL345/position2file.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# coding=utf-8
+# position2file.py
+#
+# schreibt die aus dem Beschleunigungssensor ADXL345 gelesenen Lagewerte in eine Datei
+# lediglich zur generellen Anzeige nutzen.
+# zum Wohnwagen ausrichten anderes Skript nutzen
+#
+#-------------------------------------------------------------------------------
+
+import time, datetime
+import board
+import busio
+import adafruit_adxl34x
+
+i2c = busio.I2C(board.SCL, board.SDA)
+accelerometer = adafruit_adxl34x.ADXL345(i2c)
+
+def write2file(x, y, z):
+#	try:
+		dateiName = "/home/pi/CaravanPi/sensorwerte/lage"
+		file = open(dateiName, 'a')
+		str_from_time_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+		strX = '{:.6f}'.format(x)
+		strY = '{:.6f}'.format(y)
+		strZ = '{:.6f}'.format(z)
+		file.write("\n"+ str_from_time_now + " " + strX + " " + strY + " " + strZ)
+		file.close()
+		return 0
+#	except:
+#		# Schreibfehler
+#		print ("Die Datei konnte nicht geschrieben werden.")
+#		return -1
+
+(x, y, z) = accelerometer.acceleration
+write2file(x, y, z)
---
+++ temperatur-DS1820/temp2file.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+# coding=utf-8
+# temp2file.py
+#
+# schreibt die aus den DS1820 gelesenen temperaturwerte ans Ende einer Datei 
+# pro Sensor wird eine Datei mit Zeitstemple und Temperaturwert gefüllt
+#
+#-------------------------------------------------------------------------------
+
+import os, sys, time, datetime
+
+# Global für vorhandene Temperatursensoren
+tempSensorBezeichnung = [] #Liste mit den einzelnen Sensoren-Kennungen
+tempSensorAnzahl = 0 #INT für die Anzahl der gelesenen Sensoren
+tempSensorWert = [] #Liste mit den einzelnen Sensor-Werten
+
+def ds1820einlesen():
+    global tempSensorBezeichnung, tempSensorAnzahl, programmStatus
+    #Verzeichnisinhalt auslesen mit allen vorhandenen Sensorbezeichnungen 28-xxxx
+    try:
+        for x in os.listdir("/sys/bus/w1/devices"):
+            if (x.split("-")[0] == "28") or (x.split("-")[0] == "10"):
+                tempSensorBezeichnung.append(x)
+                tempSensorAnzahl = tempSensorAnzahl + 1
+        return 0
+    except:
+        # Auslesefehler
+        print ("Der Verzeichnisinhalt konnte nicht ausgelesen werden.")
+        return -1
+
+def ds1820auslesen():
+    global tempSensorBezeichnung, tempSensorAnzahl, tempSensorWert, programmStatus
+    x = 0
+    try:
+        # 1-wire Slave Dateien gem. der ermittelten Anzahl auslesen 
+        while x < tempSensorAnzahl:
+            dateiName = "/sys/bus/w1/devices/" + tempSensorBezeichnung[x] + "/w1_slave"
+            file = open(dateiName)
+            filecontent = file.read()
+            file.close()
+            # Temperaturwerte auslesen und konvertieren
+            stringvalue = filecontent.split("\n")[1].split(" ")[9]
+            sensorwert = float(stringvalue[2:]) / 1000
+            temperatur = '%6.2f' % sensorwert #Sensor- bzw. Temperaturwert auf 2 Dezimalstellen formatiert
+            tempSensorWert.insert(x,temperatur) #Wert in Liste aktualisieren
+            x = x + 1
+    except:
+        # Fehler bei Auslesung der Sensoren
+        print ("Die Auslesung der DS1820 Sensoren war nicht möglich.")
+        programmStatus = 0
+
+def write2file(sensor, wert):
+	try:
+		dateiName = "/home/pi/CaravanPi/sensorwerte/" + sensor
+		file = open(dateiName, 'a')
+		str_from_time_now = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+		file.write("\n"+ str_from_time_now + wert)
+		file.close()
+		return 0
+	except:
+		# Schreibfehler
+		print ("Die Datei konnte nicht geschrieben werden.")
+		return -1
+
+
+
+
+# Anzahl und Bezeichnungen der vorhandenen Temperatursensoren einlesen und auswerten, falls einlesen OK
+if ds1820einlesen() == 0: 
+	# Temperaturausgabe in Schleife
+	x = 0
+	ds1820auslesen()
+	while x < tempSensorAnzahl:
+		write2file(tempSensorBezeichnung[x],tempSensorWert[x])
+		x = x + 1

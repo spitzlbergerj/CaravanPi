5d334b6d3d4fb66174271298deabaab9d5b40fc6
Josef Spitzlberger <38983450+spitzlbergerj@users.noreply.github.com>
2020-10-01 06:49:29 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,33 @@
+{
+    "author": {
+        "name": "Josef Spitzlberger"
+    },
+    "contributors": [
+    ],
+    "name": "CaravanPi",
+    "description": "Smart home functions for caravans and motor homes",
+    "version": "0.9",
+    "repository": {
+        "type": "git",
+        "url": "https://github.com/spitzlbergerj/CaravanPi.git"
+    },
+    "main": "readme.md",
+    "files": [
+        "package.json",
+        "readme.md"
+    ],
+    "keywords": [
+        "caravan",
+        "motor home",
+        "magicmirror",
+        "climate",
+        "filling level",
+        "sensor",
+        "python"
+    ],
+    "license": "MIT",
+    "readme": "CaravanPi is a smart home engine for caravans and motor homes based on raspberry pi and many sensors",
+    "readmeFilename": "README.md",
+    "devDependencies": {
+    }
+}

0ad13336450a4deaf59435e6c829ba9534551d47
Josef Spitzlberger <38983450+spitzlbergerj@users.noreply.github.com>
2022-09-01 14:47:52 +0200

    diverse Anpassungen des default config.js  aufgrund Änderungen in der MagicMirror SW

--- MagicMirror/config/config.js
+++ MagicMirror/config/config.js
@@ -7,39 +7,29 @@
 
 var config = {
 port: 8080,
-address: "0.0.0.0",
-// ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1","192.168.178.1","192.168.178.2","192.168.178.3","192.168.178.4","192.168.178.5"], 
-ipWhitelist: [], 
+address: "0.0.0.0",		// 0.0.0.0 = webserver is available within the network
+				// ipWhitelist:
+				//		first three entrys = from localhost
+				//		specific IP addresses
+				//		"::ffff:192.168.178.1/120" for all addresses 192.168.178.1 to 192.168.178.255
+				//		192.168.178.x Wohnung
+				//		192.168.168.x Caravan WLAN
+ipWhitelist: [
+		"127.0.0.1", 
+		"::ffff:127.0.0.1", 
+		"::1",
+		"::ffff:192.168.178.1/120","192.168.178.1/24",
+		"::ffff:192.168.168.1/120","192.168.168.1/24"
+], 
 language: "de",
 timeFormat: 24,
 units: "metric",
 
+useHttps: false, 		// Support HTTPS or not, default "false" will use HTTP
+httpsPrivateKey: "", 	// HTTPS private key path, only require when useHttps is true
+httpsCertificate: "", 	// HTTPS Certificate path, only require when useHttps is true
+	
 modules: [
-
-{
-	module: 'MMM-Remote-Control',
-	// uncomment the following line to show the URL of the remote control on the mirror
-	// , position: 'top_bar'
-},
-/*
-{
-	module: 'MMM-Logging',
-	config: {
-	}
-},
-*/
-
-{
-	module: 'MMM-PIR-Sensor',
-	config: {
-		sensorPin: 25,
-		sensorState: 1,
-		powerSaving: 1,
-		powerSavingDelay: 10, // 60 Sekunden bis power off
-		powerSavingNotification: true,
-		powerSavingMessage: "Bildschirm wird ausgeschaltet",
-	}
-},
 
 {
 	module: "alert",
@@ -53,8 +43,62 @@ modules: [
 	position: "top_left",
 	showWeek: "true",
 },
+{
+	module: "MMM-SimpleLogo",
+	position: "top_center",
+	config: {
+		fileUrl: "modules/MMM-SimpleLogo/public/CaravanPi-Logo.png",
+		width: "220px", 
+		position: "center",
+		text: "",
+	}
+},
 
+/* **********************************************************
+ *  nützliche Wettermodule
+ * **********************************************************
+ */
+{
+	module: "MMM-MarineWeather",
+	position: "top_right",
+	config: {
+		latitude: 43.224591562827726, // Latitude 
+		longitude: 3.247385126241382, // Longitude
+		appid: "28de0ae6-f507-11eb-9edf-0242ac130002-28de0b5e-f507-11eb-9edf-xxxxxxxxxxxx" // StormGlass API key (docs.stormglass.io)
+	}
+},
 
+{
+	module: "MMM-Sunrise-Sunset",
+	position: "top_right",
+	config: {
+		latitude: 43.224591562827726, // Latitude 
+		longitude: 3.247385126241382, // Longitude
+		apiKey: "b39e8a12da9a4355b1c3eaeexxxxxxxx",
+		layout: "inline",
+	}
+},
+
+{
+	module: "weather",
+	position: "top_right",
+	header: "Wetteraussichten",
+	config: {
+		type: "forecast",
+		roundTemp: true,
+		colored: true,
+		showPrecipitationAmount: true,
+		fade: false,
+		locationID: "6454040",  //ID from https://openweathermap.org/find
+		apiKey: "777b12391a0929fddd40310xxxxxxxxx",
+		maxNumberOfDays: 7,
+	}
+},
+
+/* **********************************************************
+ *  CaravanPi Module
+ * **********************************************************
+ */
 {
 	module: "MMM-CaravanPiClimate",
 	position: "top_left",
@@ -112,11 +156,17 @@ modules: [
 		style: "boxlines",
 		weightPrecision: 0,
 		sensors: [
-			{
-				name: "Alu 6kg Flasche",
-				file: "gasScale",
-			},
-		]
+				{
+					name: "Alu 6kg Flasche 1",
+					file: "gasScale1",
+				},
+				/*
+				{
+					name: "Alu 8kg Flasche 2",
+					file: "gasScale2",
+				},
+				*/
+			]
 	}
 },
 
@@ -135,43 +185,20 @@ modules: [
 	}
 },
 
-{
-	module: "currentweather",
-	position: "top_right",
-	config: {
-		location: "Oberschleißheim",
-		locationID: "2859147",  //ID from http://www.openweathermap.org/help/city_list.txt
-		appid: "APPID",
-		roundTemp: "true",
-		showWindDirection: "true",
-		showWindDirectionAsArrow: "true",
-		showFeelsLike: false,
-	}
-},
-
-{
-	module: "weatherforecast",
-	position: "top_right",
-	header: "Weather Forecast",
-	config: {
-		location: "Oberschleißheim",
-		locationID: "2859147",  //ID from http://www.openweathermap.org/help/city_list.txt
-		appid: "APPID",
-		roundTemp: true,
-		showRainAmount: true,
-		fade: false,
-	}
-},
-
+/* **********************************************************
+ *  weitere Module - Vorschläge
+ * **********************************************************
+ */
+	
 /*
 {
 	module: 'MMM-DWD-WarnWeather',
 	position: 'top_right',
-	header: 'Wetterwarnungen Oberschleißheim',
+	header: 'Wetterwarnungen xxxx',
 	config: {
 		displayInnerHeader: false,
 		displayRegionName: false,
-		region: 'Oberschleißheim',
+		region: 'xxxx',
 		changeColor: true,
 		minutes: false,
 		interval: 10 * 60 * 1000, // every 10 minutes
@@ -182,7 +209,28 @@ modules: [
 },
 */
 
+/*
+{
+	 module: 'MMM-GrafanaEmbedded',
+		 position: 'top_right',   // This can be any of the regions.
+		 config: {
+				/* http://192.168.178.139:3000/d/mef4p9ZVz/caravanpi-temperaturen?orgId=1&viewPanel=1 */
+				id: "mef4p9ZVz", 
+				host: "192.168.178.139", 
+				port: 3000,
+				dashboardName: "caravanpi-temperaturen",
+				orgId: 1,
+				panelId: 1,
+				width: "450",
+				height: "200",
+				refreshRate: "5m",
+				from: "now-36h",
+				to: "now",
+			}
+},
+*/
 
+/*
 {
 	module: "newsfeed",
 	position: "top_right",	// This can be any of the regions. Best results in center regions.
@@ -206,8 +254,9 @@ modules: [
 		]
 	}
 },
+*/
 
-
+/*
 {
 	module: 'MMM-SystemStats',
 	position: 'bottom_bar',
@@ -219,7 +268,18 @@ modules: [
 		singleRow: true,
 	}
 },
+*/
 
+/* **********************************************************
+ *  Remote Control Modul
+ * **********************************************************
+ */
+	
+{
+	module: 'MMM-Remote-Control',
+	// uncomment the following line to show the URL of the remote control on the mirror
+	position: 'bottom_right'
+},
 
 ] /* modules */
 
